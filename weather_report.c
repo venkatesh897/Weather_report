2ab89ed61226c04f1c287f8d9f61b4e063684514
venkatesh897 <55230656+venkatesh897@users.noreply.github.com>
2020-10-27 08:45:38 +0530

    Update weather_report.c

--- weather_report.c
+++ weather_report.c
@@ -1,56 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define FILE_NAME_OF_WEATHER_REPORT "WeatherReport.dat"
 
-void displayTemperature();
-void loadWeatherReport(char location, char fileName);
-char getTemperatureFromWeatherReport(char fileName);
+#define WEATHER_REPORT_FILE "weather_report.dat"
+
+char* get_temperature_of_city(char* file_name)
+{
+	char weather_report[1000];
+	char* temperature;
+ 	FILE* fp_weather_report = fopen(WEATHER_REPORT_FILE, "r");
+ 	fread(weather_report, sizeof(weather_report), 1, fp_weather_report);
+ 	char* weather_report_token = strtok(weather_report, "{,\":");
+ 	while(weather_report_token !=  NULL)
+ 	{
+ 		char is_temperature_found = 'n';
+ 		if(strcmp(weather_report_token, "temp"))
+ 		{
+ 			is_temperature_found = 'y';
+ 		}
+ 		weather_report_token = strtok(NULL, "{,\":");
+ 		if(is_temperature_found == 'y')
+ 		{
+ 			temperature = weather_report_token;
+ 			break;
+ 		}
+ 	}
+ 	fclose(fp_weather_report);
+ 	return temperature;
+}
+
+void display_temperature()
+{
+	char cmd[100];
+	char city_name[30];
+	printf("Enter city: ");
+	scanf("%s", city_name);
+	sprintf(cmd, "curl \"http://api.openweathermap.org/data/2.5/weather?q=%s&appid=f9ba15284b25d77cf2aae3a2733bb72a&units=metric\" > %s", city_name, WEATHER_REPORT_FILE);
+	char* temperature = get_temperature_of_city(WEATHER_REPORT_FILE);
+	printf("Temperature in %s is %s", city_name, temperature);
+}
 
 int main()
 {
-	displayTemperature();
-	return 0;
-}
-
-void loadWeatherReport(char location, char fileName)
-{
-	char command[500];
-	sprintf(cmd, "curl \"http://api.openweathermap.org/data/2.5/weather?q=%s&appid=f9ba15284b25d77cf2aae3a2733bb72a&units=metric\" > %s", location, fileName);
-	system(command);
-}
-
-char getTemperatureFromWeatherReport(char fileName)
-{
-	FILE* fpWeatherReport = fopen(fileName, "r");
-	char* temperatureInWeatherReport;
-	char weatherReport[1000];
-	fread(weatherReport, sizeof(weatherReport), 1, fpWeatherReport);
-	char* ptrParsedString = strtok(weatherReport, "{,\":");
-	while(ptrParsedString != NULL)
-	{
-		char isTemperatureFound = 'n';
-		if(strcmp(ptrParsedString, "temp") == 0)
-		{
-			isTemperatureFound = 'y';
-		}
-		ptrParsedString = strtok(NULL, "{,\":");
-		if(isTemperatureFound == 'y')
-		{
-			temperatureInWeatherReport = ptrParsedString;
-			break;
-		}
-	}
-	fclose(fpWeatherReport);
-	return temperatureInWeatherReport;
-}
-
-void displayTemperature()
-{
-	char cityName[30];
-	printf("Enter city name to find temperature: ");
-	scanf("%s", cityName);
-	loadWeatherReport(cityName, FILE_NAME_OF_WEATHER_REPORT);
-	char* temperature = getTemperatureFromWeatherReport(FILE_NAME_OF_WEATHER_REPORT);
-	printf("The temperature of %s is %s.", cityName, temperature);
+	display_temperature();
 }
